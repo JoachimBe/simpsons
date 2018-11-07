05de5fcb2dcc7af3511241293c4232e12f62dda2
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-07 13:55:16 +0100

    simpsons reac 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Lamps from "./Lamps";
 
 class App extends Component {
+  constructor(props)
+  {
+      super(props);
+      this.state = {
+        working: props.true
+      };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
   render() {
+    const working = this.state.working ? 'logo' : 'App-logo';
+    const work = this.state.working ? 'travail' : 'pause';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={working} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <button onClick={this.handleClick}  className="work">
+        {work.toUpperCase()}</button>
+        <Lamps />
+        <Quotes />
       </div>
     );
   }
